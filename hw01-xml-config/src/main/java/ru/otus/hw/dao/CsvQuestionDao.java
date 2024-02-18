12b811f62b1dbaea50c4604bf39bd6e82aaddbe2
irevin <irevin@entera.pro>
2024-02-18 17:26:02 +0300

    Реализация парсинга файла из ресурсов на основе openCSV

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,49 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+
+    @NonNull
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        try (
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())
+        ) {
+            if (inputStream == null) {
+                return Collections.emptyList();
+            }
 
-        return new ArrayList<>();
+            var questionDtoList = new CsvToBeanBuilder<QuestionDto>(new InputStreamReader(inputStream))
+                .withType(QuestionDto.class)
+                .withSeparator(';')
+                .withSkipLines(1)
+                .withExceptionHandler(e -> {
+                    throw new QuestionReadException(e.getMessage(), e);
+                })
+                .build()
+                .parse();
+
+            return questionDtoList.stream()
+                .map(QuestionDto::toDomainObject)
+                .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new QuestionReadException("Error when reading list of questions", e);
+        }
     }
 }
