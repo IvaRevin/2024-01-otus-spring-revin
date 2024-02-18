96110ca11445c0afe09f98110aa4023345a0ac71
irevin <irevin@entera.pro>
2024-02-18 17:25:04 +0300

    подтягивание контекста

--- hw01-xml-config/src/main/java/ru/otus/hw/Application.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/Application.java
@@ -1,13 +1,13 @@
 package ru.otus.hw;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.hw.service.TestRunnerService;
 
 public class Application {
     public static void main(String[] args) {
 
-        //Прописать бины в spring-context.xml и создать контекст на его основе
-        ApplicationContext context = null;
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
         var testRunnerService = context.getBean(TestRunnerService.class);
         testRunnerService.run();
 
