daae928b640e20f774fc925ae242c2e61b5c54c4
irevin <irevin@entera.pro>
2024-03-12 18:15:59 +0300

    перенос логики и тестов из прошлого дз

--- hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,8 +3,14 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -24,10 +30,44 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+
+            var answerIndex = ioService.readIntForRangeWithPrompt(
+                1,
+                question.answers().size(),
+                questionAnswersFormatter(question),
+                "Accept only numbers from 1 to " + question.answers().size()
+            );
+
+            Answer answer = question.answers().get(answerIndex - 1);
+
+            var isAnswerValid = answer.isCorrect();
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
+    }
+
+    private String questionAnswersFormatter(Question question) {
+
+        if (question == null || question.text().isBlank()) {
+            return null;
+        }
+
+        String header = String.format("%s \n", question.text());
+        if (question.answers() == null || question.answers().isEmpty()) {
+            return header;
+        }
+
+        return IntStream.range(0, question.answers().size())
+            .mapToObj(index -> {
+                Answer answer = question.answers().get(index);
+                if (answer.text() != null && !answer.text().isBlank()) {
+                    return String.format("%d) %s", index + 1, answer.text().trim());
+                } else {
+                    return null;
+                }
+            })
+            .filter(Objects::nonNull)
+            .collect(Collectors.joining("\n", header, "\n"));
     }
 
 }
