af65cc6505b2b3d12e5ee549bcdbc5f36358044d
irevin <irevin@entera.pro>
2024-03-12 19:02:52 +0300

    убрал лишние аннотации

--- hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,25 +1,22 @@
 package ru.otus.hw.config;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Locale;
 import java.util.Map;
 
-@RequiredArgsConstructor
+@Setter
 @ConfigurationProperties(prefix = "application")
 public class AppProperties implements TestConfig, TestFileNameProvider, LocaleConfig {
 
     @Getter
-    @Setter
     private int rightAnswersCountToPass;
 
     @Getter
     private Locale locale;
 
-    @Setter
     private Map<String, String> fileNameByLocaleTag;
 
     public void setLocale(String locale) {
