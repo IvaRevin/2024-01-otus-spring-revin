87d120f4430b6335b2a195e590b65ea600bdf1db
irevin <irevin@entera.pro>
2024-04-02 23:56:42 +0300

    новые сущности в jpa

--- hw06-jpa-hard/src/main/java/ru/otus/hw/models/Comment.java
+++ hw06-jpa-hard/src/main/java/ru/otus/hw/models/Comment.java
@@ -1,9 +1,12 @@
 package ru.otus.hw.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,12 +16,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "authors")
-public class Author {
+@Table(name = "comments")
+public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String fullName;
+    private String text;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id")
+    private Book book;
 }
