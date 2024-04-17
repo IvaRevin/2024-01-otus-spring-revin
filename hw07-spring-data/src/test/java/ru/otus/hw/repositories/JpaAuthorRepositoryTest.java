6a169b3ab771b8b4f7353f3fbcf2be4bf8a8ebec
irevin <irevin@entera.pro>
2024-04-17 12:37:33 +0300

    правка тестов в соотвествии со спринг датой

--- hw07-spring-data/src/test/java/ru/otus/hw/repositories/JpaAuthorRepositoryTest.java
+++ hw07-spring-data/src/test/java/ru/otus/hw/repositories/JpaAuthorRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import ru.otus.hw.models.Author;
 
 import java.util.Optional;
--- hw07-spring-data/src/test/java/ru/otus/hw/repositories/JpaCommentRepositoryTest.java
+++ hw07-spring-data/src/test/java/ru/otus/hw/repositories/JpaCommentRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Import;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Comment;
 
