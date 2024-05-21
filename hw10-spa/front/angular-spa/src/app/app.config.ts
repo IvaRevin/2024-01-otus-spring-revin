45382985cd23445e09ae9a8787245c332dca4144
irevin <irevin@entera.pro>
2024-05-22 00:05:32 +0300

    роутинг и основные модули

--- hw10-spa/front/angular-spa/src/app/app.config.ts
+++ hw10-spa/front/angular-spa/src/app/app.config.ts
@@ -2,8 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { importProvidersFrom } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), provideAnimationsAsync()]
 };
--- hw10-spa/front/angular-spa/src/app/app.routes.ts
+++ hw10-spa/front/angular-spa/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from "../app/components/book-list/book-list.component";
+import { BookEditOrCreateComponent } from "../app/components/book-edit-or-create/book-edit-or-create.component";
+import { CommentListComponent } from "../app/components/comments/comment-list.component";
+import { CommentEditComponent } from "../app/components/comment-edit/comment-edit.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'book-list', pathMatch: "full" },
+
+  { path: 'book-list', component: BookListComponent },
+  { path: 'book-edit-or-create/:id', component: BookEditOrCreateComponent },
+  { path: 'book-edit-or-create', component: BookEditOrCreateComponent },
+  { path: 'comment-list/:id', component: CommentListComponent },
+  { path: 'comment-edit', component: CommentEditComponent },
+  { path: 'comment-edit/:id', component: CommentEditComponent },
+];
