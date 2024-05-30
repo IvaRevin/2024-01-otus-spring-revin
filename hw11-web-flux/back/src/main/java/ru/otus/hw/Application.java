17514acac952567868ebf82a1832c6261088fa7f
irevin <irevin@entera.pro>
2024-05-31 01:30:47 +0300

    правка сканера компонентов

--- hw11-web-flux/back/src/main/java/ru/otus/hw/Application.java
+++ hw11-web-flux/back/src/main/java/ru/otus/hw/Application.java
@@ -1,12 +1,9 @@
 package ru.otus.hw;
 
-import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongock
-@EnableMongoRepositories
+
 @SpringBootApplication
 public class Application {
 
