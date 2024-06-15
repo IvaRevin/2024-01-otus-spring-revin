2009ea9a7f5e999f95dba0590fcbd53ff77828df
irevin <irevin@entera.pro>
2024-06-15 16:44:09 +0300

    новые даблицы аутентификации

--- hw12-spring-security/back/src/main/resources/data.sql
+++ hw12-spring-security/back/src/main/resources/data.sql
@@ -19,3 +19,12 @@ insert into comments(text, book_id)
     ('Понравилось', 1),
     ('Не понравилось', 2),
     ('Сюжет не впечатлил', 3);
+
+insert into users(username, password, enabled)
+    values ('user', '93e74353855c4cfc1b68b3732f81e1f6e73896eddfe705cbbf74c0e7c2034b8dc110d84be216bd48cbacce84a240c54e', true),
+        ('admin', '92c4f87bfc1f7fffa4bb0bf9edbaaf86543e2ca2d3068c46965c0ca5ba0b590c5d443b3dfc9fcbf0bdb20cd3600dda80', true);
+
+insert into authorities(username, authority)
+    values ('admin', 'ROLE_ADMIN'),
+       ('admin', 'ROLE_USER'),
+       ('user', 'ROLE_USER');
