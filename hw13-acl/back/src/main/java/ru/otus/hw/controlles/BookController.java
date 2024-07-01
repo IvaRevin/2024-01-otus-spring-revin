fd0c793fa40c8a92e2238fa9b928b0cd7475d081
irevin <irevin@entera.pro>
2024-07-01 17:30:56 +0300

    добавление авторизации на уровне url

--- hw13-acl/back/src/main/java/ru/otus/hw/controlles/BookController.java
+++ hw13-acl/back/src/main/java/ru/otus/hw/controlles/BookController.java
@@ -3,6 +3,7 @@ package ru.otus.hw.controlles;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,16 +29,19 @@ public class BookController {
     private final BookServiceImpl bookService;
 
     @GetMapping("/books")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public List<BookDTO> getBookList() {
         return bookService.findAll();
     }
 
     @GetMapping("/books/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public BookDTO getBook(@PathVariable(value = "id", required = false) Long id) {
         return bookService.findById(id).orElse(null);
     }
 
     @PatchMapping("/books/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(value = HttpStatus.ACCEPTED)
     public BookDTO editBook(@PathVariable("id") Long id,
                               @Valid @RequestBody BookEditDTO book) {
@@ -46,12 +50,14 @@ public class BookController {
     }
 
     @PostMapping("/books")
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(value = HttpStatus.CREATED)
     public BookDTO createBook(@Valid @RequestBody BookCreateDTO book) {
         return bookService.create(book);
     }
 
     @DeleteMapping("/books/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteBook(@PathVariable("id") Long id) {
         bookService.deleteById(id);
